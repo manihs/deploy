6b4dd0b63c2815a9292f20be739333af0c02f189
manihs <869389536q@gmail.com>
2018-12-12 11:37:30 +0530

    phase one

---
+++ app/Http/Controllers/SignupController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\{
+    Http\Request,
+    Support\Facades\DB
+};
+
+use Auth;
+
+class SignupController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    function phaseone(){
+        
+        $user = Auth::user();
+
+        $listdata = DB::table('activity_lists')
+        ->join('sub_interests', 'activity_lists.did', '=', 'sub_interests.main')
+        ->select('sub_interests.sub', 'activity_lists.did', 'sub_interests.did')
+        ->get();
+        
+        $userinterest = DB::table('user_interests')->where('uid','=', $user->id)->get()->pluck('icode');
+        $userinterest = $userinterest->toArray();
+
+        return view('interestselection', compact('listdata','userinterest'));
+    }
+}
