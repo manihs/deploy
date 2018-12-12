0c7d2ccc7905b24ef977be6375fdd1814805d195
manihs <869389536q@gmail.com>
2018-12-12 12:12:44 +0530

    sub activity

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -17,17 +17,8 @@ class SignupController extends Controller
     }
 
     function phaseone(){
-        
-        $user = Auth::user();
+       
 
-        $listdata = DB::table('activity_lists')
-        ->join('sub_interests', 'activity_lists.did', '=', 'sub_interests.main')
-        ->select('sub_interests.sub', 'activity_lists.did', 'sub_interests.did')
-        ->get();
-        dd($listdata);
-        $userinterest = DB::table('user_interests')->where('uid','=', $user->id)->get()->pluck('icode');
-        $userinterest = $userinterest->toArray();
-
-        return view('interestselection', compact('listdata','userinterest'));
+        return view('interestselection');
     }
 }
