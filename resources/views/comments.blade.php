44d62456eb7347319e7ffff8c82d68f6f0cebcf9
manihs <869389536q@gmail.com>
2018-12-21 18:25:15 +0530

    commit 2

--- resources/views/comments.blade.php
+++ resources/views/comments.blade.php
@@ -1,15 +1,55 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/customice.css') }}" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-@section('content')
-<div class="main_body">
-<button id="back">back</button>
+    <title>Document</title>
+</head>
+<body>
+<div id="back"><img src="https://img.icons8.com/ios/50/000000/circled-left.png"></div>
+<div class="comments_container">
+  <div class="c_avatars">
+    <div class="c_avatar">
+      
+    </div>
+  </div>
+  <div class="c_text">{{ $id }}</div>
+  <div class="c_icon">
+        <div class="icon"><img src="https://img.icons8.com/ios/50/000000/hearts.png"></div>
+        <div class="icon"><img src="https://img.icons8.com/ios/50/000000/reply-all-arrow.png"></div>
+  </div>  
 </div>
-<h1>{{ $id }}</h1>
-@endsection
-@section('script')
+
+<div class="bottom_cmpt">
+  <input type="text" placeholder="comment" id="inputcomment">
+  <button>send</button>
+</div>
+</body>
 <script>
 $("body").delegate("#back","click",function(){
     window.history.back();
 });
+$('#inputcomment').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        alert('You pressed a "enter" key in textbox'); 
+    //     var _token = $('input[name="_token"]').val();
+    //     $.ajax({
+    //         url : "{{ route('interest.fetch') }}",
+    //         method : "POST",
+    //         data : {comment: comment, _token: _token},
+    //         success: function(data){
+    //         alert('successfull');
+    //     }
+    // })
+    }
+});
 </script>
-@endsection
+
+</html>
+
+
